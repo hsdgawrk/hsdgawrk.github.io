1f4506551c73aa6e63880a7c1a14273755b2ec06
lzx <hsdgawrk@163.com>
2019-10-02 12:39:08 +0800

    Site updated: 2019-10-02 12:39:07

--- dist/music.js
+++ dist/music.js
@@ -4,58 +4,10 @@ const ap = new APlayer({
   autoplay: false,
   audio: [
       {
-        name: '朝汐',
-        artist: '泠鸢yousa，双笙',
-        url: ' http://music.163.com/song/media/outer/url?id=1316479227.mp3',
-        cover: 'https://p2.music.126.net/yLfVa75LLy0e3cgj4HGYaA==/109951163594033083.jpg',
-      },
-      {
-        name: '春意红包',
-        artist: '三无MarBlue,祖娅纳惜,泠鸢yousa',
-        url: ' http://music.163.com/song/media/outer/url?id=459004290.mp3',
-        cover: 'https://p1.music.126.net/TVOFJLTlo7o1hOPewr5NwA==/109951162857898547.jpg',
-      },
-      {
-        name: "君が好きだと叫びたい／好想大声说爱你（Cover BAAD）",
-        artist: '茶理理',
-        url: ' http://music.163.com/song/media/outer/url?id=1302351161.mp3',
-        cover: 'http://p2.music.126.net/_Q88InYyHiqryCbGw_jQlA==/109951163464349418.jpg',
-      },
-      {
-        name: 'Lemon（Cover 米津玄師）',
-        artist: '茶理理,人形兎',
-        url: ' http://music.163.com/song/media/outer/url?id=573747359.mp3',
-        cover: 'http://p2.music.126.net/6FG8ZczNpS3Ec_u2tit6MA==/109951163358943073.jpg',
-      },
-      {
-        name: '多情岸【忘川风华录】（Cover 忘川风华录）',
-        artist: '泠鸢yousa',
-        url: ' http://music.163.com/song/media/outer/url?id=863982546.mp3',
-        cover: 'https://p2.music.126.net/1bq_2MKtHKlFJgUgyLAkmw==/109951163387084057.jpg',
-      },
-      {
-        name: 'Fall In Love（Cover 葵井歌菜）',
-        artist: '泠鸢yousa',
-        url: ' http://music.163.com/song/media/outer/url?id=1300738492.mp3',
-        cover: 'https://p2.music.126.net/dxyUFufwkUh1TQQufkLqtw==/109951163452240678.jpg',
-      },
-      {
-        name: '怀梦之泽',
-        artist: '泠鸢yousa,银临',
-        url: ' http://music.163.com/song/media/outer/url?id=1296345669.mp3',
-        cover: 'https://p1.music.126.net/6b_OrWggmiepJV1LGPwFWg==/109951163424531701.jpg',
-      },
-      {
-        name: '[A]ddiction',
-        artist: 'EVO+',
-        url: ' http://music.163.com/song/media/outer/url?id=41654827.mp3',
-        cover: 'https://p1.music.126.net/MqPASWNd9f60mic6scZ_EA==/109951163597238996.jpg',
-      },
-      {
-        name: '君がいる世界へ',
-        artist: '鹿乃',
-        url: ' http://music.163.com/song/media/outer/url?id=1304665120.mp3',
-        cover: 'https://p1.music.126.net/bpkV1doAwRan7WA7yNlETA==/109951163509176176.jpg',
-      },
+          name: 'カタオモイ（单相思）',
+          artist: 'Aimer',
+          url: ' http://music.163.com/song/media/outer/url?id=431259256.mp3',
+          cover: 'https://p1.music.126.net/Mm5vfwgT_rkYodGW_AzWrA==/18851126858262469.jpg',
+      }
   ]
 });
